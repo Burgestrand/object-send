ae8409eb25d33f0bf5c47c16b317fda538237693
Kim Burgestrand <kim@burgestrand.se>
2010-08-01 16:33:28 +0200

    Add rspec tests for nil#send? and Object#send?

--- spec/send?_spec.rb
+++ spec/send?_spec.rb
@@ -1,7 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Send?" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "should allow send? on nil" do
+    lambda { nil.send?(:i_do_not_exist) }.should_not raise_error
+  end
+  
+  it "should take arguments and blocks" do
+    args = ['hello', 'world']
+    obj = Object.new
+    
+    def obj.i_exist(*args)
+      args
+    end
+    
+    obj.send?(:i_exist, *args).should equal args
   end
 end
