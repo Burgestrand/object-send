1337a3ae75f1f1dab254f2c6db43bb38726cfa5f
Kim Burgestrand <kim@burgestrand.se>
2010-08-01 16:46:55 +0200

    Fix test logic

--- spec/send?_spec.rb
+++ spec/send?_spec.rb
@@ -1,18 +1,33 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+class TestObject
+  
+  def i_exist(*args)
+    args
+  end
+  
+  private
+    def i_am_private(*args)
+      args
+    end
+end
+
 describe "Send?" do
   it "should allow send? on nil" do
-    lambda { nil.send?(:i_do_not_exist) }.should_not raise_error
+    nil.send?(:i_do_not_exist).should equal nil
+  end
+  
+  it "should not raise errors on non-nil objects" do
+    TestObject.new.send?(:i_do_not_exist_either).should equal nil
   end
   
   it "should take arguments and blocks" do
     args = ['hello', 'world']
-    obj = Object.new
     
-    def obj.i_exist(*args)
-      args
-    end
-    
-    obj.send?(:i_exist, *args).should equal args
+    TestObject.new.send?(:i_exist, *args).should == args
+  end
+  
+  it "should never ever call private methods" do
+    TestObject.new.send?(:i_am_private, "whatever").should == nil
   end
 end
